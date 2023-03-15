52916616ae2ba3593fdb0a94b97710cc77bb3758
Ethan Fraser <ejpfraser@gmail.com>
2023-03-15 22:28:01 +1300

    update selected card text in pb

--- notes/src/lib/CardList.svelte
+++ notes/src/lib/CardList.svelte
@@ -1,33 +1,7 @@
 <script lang="ts">
   import { pb } from "./pocketbase";
   import Card from "./Card.svelte";
-
-  let newCardKey: number = 0;
-  let allCards: any[] = [];
-  $: newCardKey = allCards.length;
-  let selectedCardKey: number | null = null;
-  let prevSelectedCardKey: number | null = 0;
-
-  let updatedCard = {
-    text: "",
-    tags: [],
-  };
-  $: if (selectedCardKey != prevSelectedCardKey) {
-    prevSelectedCardKey = selectedCardKey;
-    if (selectedCardKey !== null && selectedCardKey !== newCardKey) {
-      updatedCard.text = allCards[selectedCardKey].text;
-      updatedCard.tags = allCards[selectedCardKey].expand.tags.map(
-        (t) => t.tag
-      );
-    } else if (selectedCardKey === null) {
-      updatedCard.text = "";
-      updatedCard.tags = [];
-    }
-  }
-
-  $: {
-    console.log(updatedCard);
-  }
+  import lodash from "lodash";
 
   async function getCards() {
     return await pb
@@ -41,11 +15,64 @@
           return record.expand.tags.filter((t) => t.tag === "card").length > 0;
         })
       )
-      .then((filtered) => (allCards = filtered));
+      .then((filtered) => (allCards = filtered))
+      .catch((err) => console.error(err));
+  }
+
+  function updateSelectedCardText() {
+    if (selectedCardKey == null) return;
+    pb.collection("items")
+      .update(allCards[selectedCardKey].id, {
+        text: updatedCard.text,
+      })
+      .then((record) => {
+        allCards[selectedCardKey].text = record.text;
+      })
+      .catch((err) => console.error(err));
   }
 
   function setSelected(key: number | null) {
     selectedCardKey = key;
+  }
+
+  let newCardKey: number = 0;
+  let allCards: any[] = [];
+  $: newCardKey = allCards.length;
+  let selectedCardKey: number | null = null;
+  let prevSelectedCardKey: number | null = 0;
+  let updatedCard: {
+    text: string;
+    prevText: string | null;
+    tags: string[];
+    prevTags: string[] | null;
+  } = {
+    text: "",
+    prevText: null,
+    tags: [],
+    prevTags: null,
+  };
+
+  $: if (selectedCardKey !== prevSelectedCardKey) {
+    prevSelectedCardKey = selectedCardKey;
+    if (selectedCardKey !== null && selectedCardKey !== newCardKey) {
+      updatedCard.text = allCards[selectedCardKey].text;
+      updatedCard.tags = allCards[selectedCardKey].expand.tags.map(
+        (t) => t.tag
+      );
+    } else if (selectedCardKey === null) {
+      updatedCard.text = "";
+      updatedCard.tags = [];
+    }
+  }
+
+  $: if (updatedCard.text !== updatedCard.prevText) {
+    updatedCard.prevText = updatedCard.text;
+    updateSelectedCardText();
+  }
+
+  $: if (!lodash.isEqual(updatedCard.tags, updatedCard.prevTags)) {
+    updatedCard.prevTags = updatedCard.tags;
+    // TODO: update tags
   }
 
   getCards();
