8556b519d25a9a0f814c1dff81add76b26875a89
Ethan Fraser <ejpfraser@gmail.com>
2023-03-19 13:40:08 +1300

    only show removeTagButton if tag != card

--- notes/src/lib/TagSelector.svelte
+++ notes/src/lib/TagSelector.svelte
@@ -5,7 +5,8 @@
 <div class="tagSelector">
   {#each selectedTags as t}
     <span class="selectedTag" style="background-color: {t.color}">
-      {t.tag}<i class="fa-solid fa-x removeTagButton" />
+      {t.tag}
+      {#if t.tag !== "card"}<i class="fa-solid fa-x removeTagButton" />{/if}
     </span>
   {/each}
   <span class="addTagButton"><i class="fa-solid fa-plus" /></span>
@@ -19,15 +20,15 @@
   .selectedTag {
     display: table-cell;
     vertical-align: middle;
-    height: 25px;
+    height: 1.5em;
     color: black;
     border-radius: 8px;
-    padding: 0 10px 0 10px;
+    padding: 0 10px;
     margin-right: 10px;
   }
 
   .removeTagButton {
     font-size: 11px;
-    padding-left: 10px;
+    padding-left: 8px;
   }
 </style>
